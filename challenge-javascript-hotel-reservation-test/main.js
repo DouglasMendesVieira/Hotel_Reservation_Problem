3defdd658281f2fc1215718e0dcd12b97606c97c
Douglas Mendes <douglas.mendes.vieira@gmail.com>
2022-05-08 13:29:08 -0300

    Implemented the logic to calculate the cheapest hotel

--- challenge-javascript-hotel-reservation-test/main.js
+++ challenge-javascript-hotel-reservation-test/main.js
@@ -1,5 +1,40 @@
-function getCheapestHotel (input) { //DO NOT change the function's name.
-  return "Cheapest hotel name"
+const CATEGORIES = require("./src/configs/user.config");
+
+const betterHotelOption = (user, hotels) => {
+  const userType = user.getType();
+  const dailies = user.getDaily();
+  var hotelPrices = [];
+
+  hotels.forEach((hotel) => {
+    var rateSum = 0.0;
+
+    dailies.forEach((daily) => {
+      if (userType === CATEGORIES.REGULAR) {
+        rateSum += daily.getWeekDay()
+          ? hotel.getWeekRate().getRegularRate()
+          : hotel.getWeekendRate().getRegularRate();
+      } else if (userType === CATEGORIES.REWARD) {
+        rateSum += daily.getWeekDay()
+          ? hotel.getWeekRate().getRewardRate()
+          : hotel.getWeekendRate().getRewardRate();
+      }
+    });
+    hotelPrices.push({ hotel, rateSum });
+  });
+
+  return getCheapestHotel(hotelPrices);
+};
+
+function getCheapestHotel(input) {
+  //DO NOT change the function's name.
+  const betterHotel = input.reduce((previous, current) =>
+    previous.rateSum < current.rateSum ? previous : current
+  );
+
+  console.log("\x1b[35m%s\x1b[0m", "\n*** Hotel disponível mais barato! ***");
+  console.log(betterHotel.hotel.getName());
+
+  return betterHotel.hotel.getName();
 }
 
-exports.getCheapestHotel = getCheapestHotel
+module.exports = { betterHotelOption, getCheapestHotel };
---
+++ challenge-javascript-hotel-reservation-test/test/index.js
@@ -0,0 +1,1 @@
+require("./failing-test-spec");
